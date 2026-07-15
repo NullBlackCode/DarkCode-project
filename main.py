04008bfe1fd173a1267648f0426ffb50097c8dd9
BlackCode <amirevercode@gmail.com>
2026-07-15 12:43:50 -0400

    update

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import core.menu_main
 import modules.crack_modules.menu_crack
+import modules.web_modules.menu_web
 import os
 from colorama import Fore
 import time
+def clear_screen():
+    os.system("clear")
+
 def main():
     while True:
         try:
-            os.system("clear" or "cls")
+            clear_screen()
             print(core.menu_main.banner_main)
             user_enter = int(input(Fore.RED + "~>"))
             if user_enter == 1:
-                os.system("clear" or "cls")
+                clear_screen()
                 print(Fore.LIGHTBLUE_EX + "Please white...")
-                time.sleep(2)
+                time.sleep(0.60)
                 modules.crack_modules.menu_crack.main.main_code()
+                break
+            elif user_enter == 2:
+                clear_screen()
+                print(Fore.LIGHTBLUE_EX + "Please white...")
+                time.sleep(0.60)
+                modules.web_modules.menu_web.main.main_code()
                 break
             else:
                 print(Fore.RED + "Not Number try again (ENTER)")
